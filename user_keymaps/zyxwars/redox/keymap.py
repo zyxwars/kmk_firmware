365ade4f50fe2969078b726f9df81d58e31ae0f0
zyxwars <zyxwars221@gmail.com>
2022-03-13 01:36:17 +0100

    Add vim mode

--- user_keymaps/zyxwars/redox/keymap.py
+++ user_keymaps/zyxwars/redox/keymap.py
@@ -14,8 +14,8 @@ SFTBSLS = KC.MT(KC.BSLS, KC.RSFT)
 SFTESC = KC.MT(KC.ESC, KC.LSFT)
 F5RUN = KC.TD(KC.F5, KC.LCTL(KC.F5))
 NXTPRV = KC.TD(KC.MNXT, KC.MPRV)
-VIMIN = KC.TD(KC.U, KC.TG(4))
-VIMOUT = KC.TO(0)
+VIMIN = KC.TM(4, KC.LSFT)
+VIMOUT = KC.TG(4)
 
 CLOSE = KC.LALT(KC.F4)
 FILES = KC.LGUI(KC.E)
@@ -60,8 +60,8 @@ keymap = [
         KC.GRV,  KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,   F5RUN,          GAMING,  KC.N6,   KC.N7,   KC.N8,   KC.N9,   KC.N0,   KC.MINUS,
         KC.TAB,  KC.Q,    KC.W,    KC.F,    KC.P,    KC.B,    KC.LBRC,        KC.RBRC, KC.J,    KC.L,    KC.U,    KC.Y,    KC.SCLN, KC.EQUAL,
         KC.BSPC, KC.A,    KC.R,    KC.S,    KC.T,    KC.G,    GITCA,          _______, KC.M,    KC.N,    KC.E,    KC.I,    KC.O,    KC.QUOT,
-        SFTESC,  KC.Z,    KC.X,    KC.C,    KC.D,    KC.V,    KC.RESET,       _______, KC.K,    KC.H,    KC.COMMA,KC.DOT,  KC.SLASH,SFTBSLS,
-        KC.LCTL, GUI,     KC.LALT, KC.F12,  FN,      KC.SPC,  KC.DEL,         ENTFN,   _______, FN,      KC.LEFT, KC.DOWN, KC.UP,   KC.RIGHT
+        VIMIN,   KC.Z,    KC.X,    KC.C,    KC.D,    KC.V,    KC.RESET,       _______, KC.K,    KC.H,    KC.COMMA,KC.DOT,  KC.SLASH,SFTBSLS,
+        KC.LCTL, GUI,     KC.LALT, KC.F12,  FN,      KC.SPC,  KC.DEL,         ENTFN,   VIMOUT,  FN,      KC.LEFT, KC.DOWN, KC.UP,   KC.RIGHT
     ],
     [   # FN
         _______, KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.F5,   _______,        _______, KC.F6,   KC.F7,   KC.F8,   KC.F9,   KC.F10,  KC.F11,
@@ -86,9 +86,9 @@ keymap = [
     ],
     [   # VIM
         _______, _______, _______, _______, _______, _______, _______,       _______,  _______, _______, _______, _______, _______, _______,
-        _______, KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,    KC.H,          KC.RBRC, KC.Y,    KC.U,    KC.I,     KC.O,    KC.P,    KC.EQUAL,
-        _______, KC.A,    KC.S,    KC.D,    KC.F,    KC.G,    KC.M,          _______, KC.LEFT, KC.DOWN, KC.UP,    KC.RIGHT,KC.SCLN, KC.QUOT,
-        VIMOUT,  KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,    _______,       _______, KC.N,    KC.M,    KC.COMMA, KC.DOT,  KC.SLASH,SFTBSLS,
+        _______, KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,    _______,       _______, KC.Y,    KC.U,     VIMOUT,  KC.O,    KC.P,    KC.EQUAL,
+        _______, KC.A,    KC.S,    KC.D,    KC.F,    KC.G,    _______,       _______, KC.LEFT, KC.DOWN,  KC.UP,   KC.RIGHT,KC.SCLN, KC.QUOT,
+        _______, KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,    _______,       _______, KC.N,    KC.M,     KC.COMMA,KC.DOT,  KC.SLASH,SFTBSLS,
         _______, _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______, _______,
     ]
 ]
