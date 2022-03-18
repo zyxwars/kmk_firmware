76c4176f54b7033908eeab850203091d0aab58f2
zyxwars <zyxwars221@gmail.com>
2022-03-18 17:59:22 +0100

    Update keymap

--- user_keymaps/zyxwars/redox/keymap.py
+++ user_keymaps/zyxwars/redox/keymap.py
@@ -10,9 +10,10 @@ GUI = KC.LM(2, KC.LGUI)
 FN = KC.MO(1)
 ENTFN = KC.LT(1, KC.ENT, prefer_hold=True, tap_interrupted=False)
 GAMING = KC.TG(3)
+NUM = KC.TG(4)
 
 # Multi
-SFTMNS = KC.MT(KC.MINUS, KC.RSFT)
+SFTEQL = KC.MT(KC.EQUAL, KC.RSFT)
 SFTESC = KC.MT(KC.ESC, KC.LSFT)
 F5RUN = KC.TD(KC.F5, KC.LCTL(KC.F5))
 NXTPRV = KC.TD(KC.MNXT, KC.MPRV)
@@ -38,16 +39,16 @@ GITCA = simple_key_sequence(
     )
 )
 
-# Home row mods
-# hrm_time = 150
-# A = KC.MT(KC.A, KC.LGUI, prefer_hold=False, tap_time=200, tap_interrupted=False)
-# R = KC.MT(KC.R, KC.LALT, prefer_hold=False, tap_time=hrm_time, tap_interrupted=False)
-# S = KC.MT(KC.S, KC.LSFT, prefer_hold=False, tap_time=hrm_time, tap_interrupted=False)
-# T = KC.MT(KC.T, KC.LCTL, prefer_hold=False, tap_time=hrm_time, tap_interrupted=False)
-# N = KC.MT(KC.N, KC.RCTL, prefer_hold=False, tap_time=hrm_time, tap_interrupted=False)
-# E = KC.MT(KC.E, KC.RSFT, prefer_hold=False, tap_time=hrm_time, tap_interrupted=False)
-# I = KC.MT(KC.I, KC.RALT, prefer_hold=False, tap_time=hrm_time, tap_interrupted=False)
-# O = KC.MT(KC.O, KC.RGUI, prefer_hold=False, tap_time=200, tap_interrupted=False)
+S1 = KC.LSFT(KC.N1)
+S2 = KC.LSFT(KC.N2)
+S3 = KC.LSFT(KC.N3)
+S4 = KC.LSFT(KC.N4)
+S5 = KC.LSFT(KC.N5)
+S6 = KC.LSFT(KC.N6)
+S7 = KC.LSFT(KC.N7)
+S8 = KC.LSFT(KC.N8)
+S9 = KC.LSFT(KC.N9)
+S0 = KC.LSFT(KC.N0)
 
 keymap = [
     #[   # QWERTY
@@ -59,10 +60,10 @@ keymap = [
     #],
     [   # COLEMAK-DH
         KC.GRV,  KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,   F5RUN,          GAMING,  KC.N6,   KC.N7,   KC.N8,   KC.N9,   KC.N0,   KC.BSLS,
-        KC.TAB,  KC.Q,    KC.W,    KC.F,    KC.P,    KC.B,    KC.F12,         _______, KC.J,    KC.L,    KC.U,    KC.Y,    KC.SCLN, KC.EQUAL,
+        KC.TAB,  KC.Q,    KC.W,    KC.F,    KC.P,    KC.B,    KC.F12,         _______, KC.J,    KC.L,    KC.U,    KC.Y,    KC.SCLN, KC.MINUS,
         KC.BSPC, KC.A,    KC.R,    KC.S,    KC.T,    KC.G,    GITCA,          _______, KC.M,    KC.N,    KC.E,    KC.I,    KC.O,    KC.QUOT,
-        SFTESC,  KC.Z,    KC.X,    KC.C,    KC.D,    KC.V,    KC.RESET,       _______, KC.K,    KC.H,    KC.COMMA,KC.DOT,  KC.SLASH,SFTMNS,
-        KC.LCTL, GUI,     KC.LALT, KC.F12,  FN,      KC.SPC,  KC.DEL,         ENTFN,   ENTFN,   FN,      KC.LEFT, KC.DOWN, KC.UP,   KC.RIGHT
+        SFTESC,  KC.Z,    KC.X,    KC.C,    KC.D,    KC.V,    KC.RESET,       _______, KC.K,    KC.H,    KC.COMMA,KC.DOT,  KC.SLASH,SFTEQL,
+        KC.LCTL, GUI,     KC.LALT, KC.F12,  FN,      KC.SPC,  KC.DEL,         NUM,     ENTFN,   FN,      KC.LEFT, KC.DOWN, KC.UP,   KC.RIGHT
     ],
     [   # FN
         _______, KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.F5,   _______,        _______, KC.F6,   KC.F7,   KC.F8,   KC.F9,   KC.F10,  KC.F11,
@@ -83,6 +84,13 @@ keymap = [
         KC.TAB,  KC.Q,    KC.W,    KC.E,    KC.R,   KC.T,     KC.H,           KC.RBRC, KC.Y,    KC.U,    KC.I,    KC.O,    KC.P,    _______,
         KC.BSPC, KC.A,    KC.S,    KC.D,    KC.F,   KC.G,     KC.M,           _______, KC.H,    KC.J,    KC.K,    KC.L,    KC.SCLN, _______,
         KC.LSFT, KC.Z,    KC.X,    KC.C,    KC.V,   KC.B,     _______,        _______, KC.N,    KC.M,    KC.COMMA,KC.DOT,  KC.SLASH,_______,
+        _______, _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______, _______,
+    ],
+     [   # NUM
+        _______, _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______, _______,
+        _______, S1,      S2,      S3,      S4,      S5,      _______,        _______, S6,      S7,      S8,       S9,     S0,      _______,
+        _______, KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,   _______,        _______, KC.N6,   KC.N7,   KC.N8,   KC.N9,   KC.N0,   _______,
+        _______, _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______, _______,
     ],
 ]
